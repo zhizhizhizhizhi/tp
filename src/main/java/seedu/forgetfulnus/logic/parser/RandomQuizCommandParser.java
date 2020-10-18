9bf80a221e8c2720eb27a12d07ca436c1368d336
kswk <kswk@live.com>
2020-10-18 21:26:39 +0800

    Fix checkstyle violations

--- src/main/java/seedu/forgetfulnus/logic/parser/RandomQuizCommandParser.java
+++ src/main/java/seedu/forgetfulnus/logic/parser/RandomQuizCommandParser.java
@@ -3,21 +3,24 @@ package seedu.forgetfulnus.logic.parser;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import static seedu.forgetfulnus.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
 import seedu.forgetfulnus.commons.core.index.Index;
 import seedu.forgetfulnus.logic.commands.RandomQuizCommand;
 import seedu.forgetfulnus.logic.parser.exceptions.ParseException;
 
-import static seedu.forgetfulnus.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-
 /**
- * Parses the given {@code String} of arguments in the context of the RandomQuizCommand
- * and returns a RandomQuizCommand object for execution.
- * @throws ParseException if the user input does not conform the expected format
+ * Parses input arguments and creates a new RandomQuizCommand object.
  */
 public class RandomQuizCommandParser implements Parser<RandomQuizCommand> {
 
     private static Logger logger = Logger.getLogger("RandomQuizCommandParserLogger");
 
+    /**
+     * Parses the given {@code String} of arguments in the context of the RandomQuizCommand
+     * and returns a RandomQuizCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
     public RandomQuizCommand parse(String args) throws ParseException {
         logger.log(Level.INFO, "Start processing arguments");
         try {
