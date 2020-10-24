24d973025296dd3d4b1ea5fe926440d995a11de8
kswk <kswk@live.com>
2020-10-24 20:35:03 +0800

    Add test cases

--- src/test/java/seedu/forgetfulnus/logic/commands/RandomQuizCommandTest.java
+++ src/test/java/seedu/forgetfulnus/logic/commands/RandomQuizCommandTest.java
@@ -1,39 +1,43 @@
 package seedu.forgetfulnus.logic.commands;
 
-import static seedu.forgetfulnus.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.forgetfulnus.testutil.Assert.assertThrows;
 import static seedu.forgetfulnus.testutil.TypicalFlashCards.getTypicalGlossary;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import seedu.forgetfulnus.commons.core.Messages;
+import seedu.forgetfulnus.commons.core.index.Index;
+import seedu.forgetfulnus.logic.commands.exceptions.CommandException;
+import seedu.forgetfulnus.model.Glossary;
 import seedu.forgetfulnus.model.Model;
 import seedu.forgetfulnus.model.ModelManager;
 import seedu.forgetfulnus.model.UserPrefs;
-import seedu.forgetfulnus.model.flashcard.FlashCard;
 
 /**
- * Contains integration tests (interaction with the Model) and unit tests for
+ * Contains integration test (interaction with the Model) and unit test for
  * {@code RandomQuizCommand}.
  */
 public class RandomQuizCommandTest {
 
-    private Model model;
-    private Model expectedModel;
+    private Model model = new ModelManager(getTypicalGlossary(), new UserPrefs());
 
-
-    @BeforeEach
-    public void setUp() {
-        model = new ModelManager(getTypicalGlossary(), new UserPrefs());
-        expectedModel = new ModelManager(getTypicalGlossary(), new UserPrefs());
+    @Test
+    public void execute_randomNonEmptyList_success() throws CommandException {
+        String expectedMessage = RandomQuizCommand.MESSAGE_SUCCESS;
+        String actualMessage = new RandomQuizCommand(
+                Index.fromOneBased(getTypicalGlossary().getFlashCardList().size()))
+                .execute(model)
+                .getFeedbackToUser();
+        assertEquals(expectedMessage, actualMessage);
     }
 
     @Test
-    public void execute_listIsNotFiltered_hideAllEnglishWords() {
-        for (FlashCard flashCard: expectedModel.getFilteredFlashCardList()) {
-            FlashCard changeTo = flashCard.copy();
-            changeTo.updateShowingEnglish(false);
-            expectedModel.setFlashCard(flashCard, changeTo);
-        }
-        assertCommandSuccess(new QuizCommand(), model, QuizCommand.MESSAGE_SUCCESS, expectedModel);
+    public void execute_randomEmptyList_throwsCommandException() {
+        model.setGlossary(new Glossary());
+        assertThrows(
+                CommandException.class,
+                Messages.MESSAGE_INVALID_FLASHCARD_DISPLAYED_INDEX, () -> new RandomQuizCommand(
+                        Index.fromOneBased(1)).execute(model));
     }
 }
